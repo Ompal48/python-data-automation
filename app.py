6346dbc281214c9db1eadad212c5c16f8d159d90
Ompal48 <ompaljd@gmail.com>
2026-02-08 02:34:41 +0530

    python-data-automation

--- app.py
+++ app.py
@@ -75,5 +75,6 @@ def report():
     return render_template('report.html', table_columns=table_columns, table_data=table_data, summary_text=summary_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
